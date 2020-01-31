61ca87e1e11062649bef88e6d492c0dcda4a5d57
xu_bighead@foxmail.com <xu_bighead@foxmail.com>
2020-01-31 21:36:47 +0800

    feat: 添加获取鼠标焦点

--- src/views/bookmark/index.vue
+++ src/views/bookmark/index.vue
@@ -6,29 +6,63 @@
           <span class="el-dropdown-link">影视资源</span>
           <el-dropdown-menu slot="dropdown">
             <el-dropdown-item>BiliBili</el-dropdown-item>
-            <el-dropdown-item>嘀哩嘀哩</el-dropdown-item>
-            <el-dropdown-item>电影天堂</el-dropdown-item>
+            <el-dropdown-item>嘀哩嘀哩1</el-dropdown-item>
+            <el-dropdown-item>嘀哩嘀哩2</el-dropdown-item>
+            <el-dropdown-item>嘀哩嘀哩3</el-dropdown-item>
+            <el-dropdown-item>嘀哩嘀哩4</el-dropdown-item>
+            <el-dropdown-item>嘀哩嘀哩5</el-dropdown-item>
+            <el-dropdown-item>嘀哩嘀哩6</el-dropdown-item>
+            <el-dropdown-item>嘀哩嘀哩7</el-dropdown-item>
+            <el-dropdown-item>嘀哩嘀哩8</el-dropdown-item>
+            <el-dropdown-item>电影天堂9</el-dropdown-item>
             <el-dropdown-item>
               <div>
                 <el-button type="text" @click="openURL()">人人影视</el-button>
                 <el-button type="text" icon="el-icon-delete" circle/>
               </div>
             </el-dropdown-item>
-            <el-dropdown-item divided>黑夜影视</el-dropdown-item>
+            <el-dropdown-item>
+              <div>
+                <el-button type="text" @mouseenter.native="openURL()">黑夜影视</el-button>
+                <el-button type="text" icon="el-icon-delete" circle/>
+              </div>
+            </el-dropdown-item>
+            <el-dropdown-item divided>添加新书签</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </el-button>
     </el-col>
-    <el-col :span="8"><div class="grid-content bg-purple">2</div></el-col>
-    <el-col :span="8"><div class="grid-content bg-purple">3</div></el-col>
+    <el-col :span="8"><el-button type="text" icon="el-icon-folder-opened">
+      <el-dropdown>
+        <span class="el-dropdown-link">影视资源</span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item> <ul v-infinite-scroll="load" class="infinite-list" style="overflow:auto">
+            <li class="infinite-list-item" >1</li>
+            <li class="infinite-list-item" >1</li>
+            <li class="infinite-list-item" >1</li>
+            <li class="infinite-list-item" >1</li>
+            <li class="infinite-list-item" >1</li>
+          </ul></el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-button></el-col>
+    <el-col :span="8"><el-button type="text" icon="el-icon-folder-opened" @mouseenter.native="openURL">游戏资源</el-button></el-col>
   </el-row>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
   methods: {
+    load() {
+      this.count += 2
+    },
     openURL() {
       console.log('this is log')
-      window.open('https://www.baidu.com/s?&wd=' + this.input, '_blank')
+      // window.open('https://www.baidu.com/s?&wd=' + this.input, '_blank')
     }
   }
 }
